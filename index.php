066e4ba0b80cd6b6e8d587c0f80baa82ec5bfe91
alerto <albertomarido@hotmail.com>
2014-08-16 23:16:54 +0200

    plantilla crreada

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>LAM robotica</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <script src="js/vendor/modernizr-2.8.0.min.js"></script>
+
+        <link rel="stylesheet" href="js/vendor/jquery.mobile-1.4.2/jquery.mobile-1.4.2.css" />
+        <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
+        <link rel="stylesheet" href="js/vendor/jquery.mobile-1.4.2/jquery.mobile.structure-1.4.2.css" />
+
+        <script type="text/javascript" src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/vendor/jquery.localize.js"></script>
+        <script src="js/main.js"></script>
+        <script type="text/javascript" src="js/vendor/jquery.mobile-1.4.2/jquery.mobile-1.4.2.js"></script>
+
+        <link rel="stylesheet" href="css/themes/themes.css" />
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/home.css">
+        <script src="js/plugins.js"></script>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+        <?php
+            include './footer.html';
+        /*
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+         */
+        ?>
+    </body>
+</html>
---
+++ plantilla.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Web corporativa dedicada..."/>
+        <meta name="keywords" content="robotic, robotica"/>
+        <meta name="robots" content="..."/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>LAM robotica</title>        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <script src="js/vendor/modernizr-2.8.0.min.js"></script>
+
+        <link rel="stylesheet" href="js/vendor/jquery.mobile-1.4.2/jquery.mobile-1.4.2.css" />
+        <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
+        <link rel="stylesheet" href="js/vendor/jquery.mobile-1.4.2/jquery.mobile.structure-1.4.2.css" />
+
+        <script type="text/javascript" src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/vendor/jquery.localize.js"></script>
+        <script type="text/javascript" src="js/vendor/jquery.mobile-1.4.2/jquery.mobile-1.4.2.js"></script>
+        <link rel="stylesheet" href="css/themes/themes.css" />
+        <script src="js/plugins.js"></script>
+
+        <script src="js/main.js"></script>
+        <script src="js/home.js"></script>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/home.css">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div data-role="page" data-theme = "f">
+            
+            <?php
+            include_once './header_1.html';
+            ?>
+
+            <?php
+            include_once './footer.html';
+            ?>
+        </div>
+
+    </body>
+</html>
